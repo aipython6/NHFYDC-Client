5f9b6cad26d92391ed24ec4104c4cf9f92d6e469
nongdingan <1146952400@qq.com>
2023-06-07 21:03:30 +0800

    完善个人设置页面

--- src/views/account/settings.vue
+++ src/views/account/settings.vue
@@ -1,1 +1,17 @@
-<template> <div> 个人中心 </div> </template>
+<script setup lang="ts">
+  import { useUserStore } from '@/store/modules/user';
+  const userStore = useUserStore();
+  console.log(userStore.userInfo);
+  const src = 'http://localhost:3010/avatar/images/default.jfif';
+</script>
+<template>
+  <div class="text-gray-700 font-bold text-xl flex justify-center">
+    <span>个人中心</span>
+  </div>
+  <div class="flex align-center justify-center pt-8">
+    <div>
+      <a-image :src="src" :width="120"> </a-image>
+    </div>
+    <div>{{ userStore.userInfo.departmentName }}</div>
+  </div>
+</template>
